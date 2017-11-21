470d0bbccd2ffa63fda44ce2f061717d3f86e215
Niko <n.x.peralta@gmail.com>
2017-11-20 20:45:20 -0500

    tryna make a standalone text generator. no work so far.

---
+++ generator.py
@@ -0,0 +1,78 @@
+'''
+GOLD TEAM RULES
+'''
+
+from __future__ import print_function
+from keras.models import Sequential
+from keras.models import load_model
+from keras.layers import Dense, Activation
+from keras.layers import LSTM
+from keras.optimizers import RMSprop
+import numpy as np
+import random
+import sys
+
+path = 'lstmIn.txt'
+text = open(path, encoding="utf8").read()
+print('corpus length:', len(text))
+#map ascii characters to phonemes
+
+chars = sorted(list(set(text)))
+print('total chars:', len(chars))
+char_indices = dict((c, i) for i, c in enumerate(chars))
+indices_char = dict((i, c) for i, c in enumerate(chars))
+
+# cut the text in semi-redundant sequences of maxlen characters
+maxlen = 40
+step = 3
+sentences = []
+next_chars = []
+for i in range(0, len(text) - maxlen, step):
+    sentences.append(text[i: i + maxlen])
+    next_chars.append(text[i + maxlen])
+print('nb sequences:', len(sentences))
+
+print('Vectorization...')
+x = np.zeros((len(sentences), maxlen, len(chars)), dtype=np.bool)
+y = np.zeros((len(sentences), len(chars)), dtype=np.bool)
+for i, sentence in enumerate(sentences):
+    for t, char in enumerate(sentence):
+        x[i, t, char_indices[char]] = 1
+    y[i, char_indices[next_chars[i]]] = 1
+
+
+# load model
+model = load_model("models/model_04000")
+
+def sample(preds, temperature=1.0):
+    # helper function to sample an index from a probability array
+    preds = np.asarray(preds).astype('float64')
+    preds = np.log(preds) / temperature
+    exp_preds = np.exp(preds)
+    preds = exp_preds / np.sum(exp_preds)
+    probas = np.random.multinomial(1, preds, 1)
+    return np.argmax(probas)
+
+# generate text
+diversity = 0.5
+generated = ''
+sentence = '25/ nL|a nCp K ^X |#]1 0L} |P/(a '
+generated += sentence
+print('----- Generating with seed: "' + sentence + '"')
+sys.stdout.write(generated)
+
+for i in range(400):
+    x_pred = np.zeros((1, maxlen, len(chars)))
+    for t, char in enumerate(sentence):
+        x_pred[0, t, char_indices[char]] = 1.
+
+    preds = model.predict(x_pred, verbose=0)[0]
+    next_index = sample(preds, diversity)
+    next_char = indices_char[next_index]
+
+    generated += next_char
+    sentence = sentence[1:] + next_char
+
+    sys.stdout.write(next_char)
+    sys.stdout.flush()
+print()
